d47a2d32f05701f4cb90f75d5357c37abcf7938c
hgmranasinghe <hgmenukaranasinghe@gmail.com>
2025-09-30 19:10:17 +0530

    frontend

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, render_template, jsonify
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import os
+import tensorflow as tf
+
+print(tf.__version__)
+print("Imports successful")
+
+app = Flask(__name__)
+
+# Load trained models
+cnn_model = load_model('Plant_Disease_Model1.keras')   # CNN model
+tl_model = load_model('Plant_Disease_Model2.keras')    # Transfer Learning model
+
+class_names = [
+    'Pepper__bell___Bacterial_spot', 'Pepper__bell___healthy',
+    'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy',
+    'Tomato_Bacterial_spot', 'Tomato_Early_blight', 'Tomato_Late_blight',
+    'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot',
+    'Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato__Target_Spot',
+    'Tomato__Tomato_YellowLeaf__Curl_Virus', 'Tomato__Tomato_mosaic_virus',
+    'Tomato_healthy'
+]
+
+def prepare_image(img_path, target_size=(128, 128)):
+    img = image.load_img(img_path, target_size=target_size)
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0) / 255.0
+    return img_array
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file uploaded'}), 400
+
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No file selected'}), 400
+
+    filepath = os.path.join('uploads', file.filename)
+    os.makedirs('uploads', exist_ok=True)
+    file.save(filepath)
+
+    img = prepare_image(filepath)
+
+    # Choose which model to use (example: CNN)
+    prediction = np.argmax(cnn_model.predict(img), axis=1)[0]  
+    predicted_class = class_names[prediction]
+
+    return jsonify({'prediction': predicted_class})
+
+if __name__ == "__main__":
+    app.run(debug=True)
